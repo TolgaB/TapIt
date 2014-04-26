11bf805f6889d3713e42b511b70e4d440a3f5787
TolgaB <mathman290@gmail.com>
2014-04-26 10:24:28 -0700

    Added Score Screen
    Timing added, Score screen added

--- Projectfiles/HelloWorldLayer.m
+++ Projectfiles/HelloWorldLayer.m
@@ -1,7 +1,7 @@
 /*
  * Kobold2D™ --- http://www.kobold2d.org
  *
- * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Copyright (c) 2010-2011 Steffen Itterheim.
  * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
  */
 
@@ -12,10 +12,11 @@
 
 @implementation HelloWorldLayer
 
-int score;
+int score= 0;
+int latestScore= 0;
 
 CCLabelTTF* title;
-CCSprite *scoreboard;
+CCLabelTTF *scoreboard;
 
 
 -(id) init
@@ -26,8 +27,8 @@ CCSprite *scoreboard;
         
 		// "empty" as in "minimal code & resources"
 		//CCLabelTTF* label = [CCLabelTTF labelWithString:@"Minimal Kobold2D Project"
-			//								   fontName:@"Arial"
-			//								   fontSize:20];
+        //								   fontName:@"Arial"
+        //								   fontSize:20];
 		//label.position = [CCDirector sharedDirector].screenCenter;
 		//label.color = ccCYAN;
 		//[self addChild:label];
@@ -37,35 +38,56 @@ CCSprite *scoreboard;
         [self addChild:title];
         score = 0;
         [self scheduleUpdate];
+        NSTimer* myTimer = [NSTimer scheduledTimerWithTimeInterval:3 target:self selector:@selector(timeisup) userInfo:nil repeats:NO];
         
-    
         
 	}
-
+    
 	return self;
 }
 -(void) update:(ccTime)dt
 {
-  
-    [self addChild:title];
-    [self addChild:scoreboard];
-    [self score];
-
+    
+    
+    if(score >= 0) {
+        [self score];
+    }
+    
 }
+
+
+-(void) timeisup
+{
+    
+    [self removeChild:scoreboard cleanup:YES];
+    latestScore= score;
+    score= -1;
+    if (score == -1) {
+        NSLog(@"Latest Score Added");
+        NSString *scorestring = [[NSString alloc] initWithFormat: @"%i", latestScore];
+        scoreboard = [CCLabelTTF labelWithString:scorestring fontName:@"Arial" fontSize:32];
+        scoreboard.position = ccp(160, 300);
+        [self addChild:scoreboard];
+    }
+}
+
 -(void) score
 {
     
     
     if ([KKInput sharedInput].touchesAvailable)
     {
+        [self removeChild:scoreboard cleanup:YES];
         NSLog(@"Touch Detected");
         score++;
-        NSString *scorestring = [[NSString alloc] initWithFormat: @"%i strong", score];
-        CCLabelTTF* scoreboard = [CCLabelTTF labelWithString:scorestring fontName:@"Arial" fontSize:32];
+        NSString *scorestring = [[NSString alloc] initWithFormat: @"%i", score];
+        scoreboard = [CCLabelTTF labelWithString:scorestring fontName:@"Arial" fontSize:32];
         scoreboard.position = ccp(160, 300);
         [self addChild:scoreboard];
         
     }
 }
+
+
 
 @end
